88fa98c1c7c310a2632feb8ffa444d71f29663bd
Sodik <fikrijatim1405@gmail.com>
2020-11-22 11:16:04 +0700

    fisrt commit

---
+++ assets/js/myjs.js
@@ -0,0 +1,69 @@
+$('#tumpeng').on('click', function(){
+    $('#modal-img').attr('src', 'assets/img/tumpng.jpg')
+    $('#nama-item').html("Nasi Tumpeng")
+    $('#harga-item').html('Rp. 200.000')
+    $('#item-desc').html("Nasi Tumpeng Komplit")
+})
+
+$('#bebek').on('click', function(){
+    $('#modal-img').attr('src', 'assets/img/bebek-raja.jpg')
+    $('#nama-item').html("Bebek Raja Bali")
+    $('#harga-item').html('Rp. 200.000')
+    $('#item-desc').html("Bebek Raja Bali Dengan Tambahan Sambel Matah")
+})
+
+$('#ayam').on('click', function(){
+    $('#modal-img').attr('src', 'assets/img/ayam.jpg')
+    $('#nama-item').html("Ayam Bakar Super")
+    $('#harga-item').html('Rp. 100.000')
+    $('#item-desc').html("Tambahan Sambel Ijo")
+})
+
+$('#sup').on('click', function(){
+    $('#modal-img').attr('src', 'assets/img/sup-kari.JPG')
+    $('#nama-item').html("Sup Kari Ayam")
+    $('#harga-item').html('Rp. 30.000')
+    $('#item-desc').html("Super Pedas")
+})
+
+$('#nasgor').on('click', function(){
+    $('#modal-img').attr('src', 'assets/img/nasi-goreng-lahm.jpg')
+    $('#nama-item').html("Nasgor Lahm")
+    $('#harga-item').html('Rp. 10.000')
+    $('#item-desc').html("Komplit")
+})
+
+$('#bakso').on('click', function(){
+    $('#modal-img').attr('src', 'assets/img/bakso-kikil.jpg')
+    $('#nama-item').html("Bakso Kikil")
+    $('#harga-item').html('Rp. 20.000')
+    $('#item-desc').html("Komplit")
+})
+
+$('#nasirames').on('click', function(){
+    $('#modal-img').attr('src', 'assets/img/nasi-rames.jpg')
+    $('#nama-item').html("Nasi Rames")
+    $('#harga-item').html('Rp. 10.000')
+    $('#item-desc').html("Komplit")
+})
+
+$('#tunjang').on('click', function(){
+    $('#modal-img').attr('src', 'assets/img/tunjang.jpg')
+    $('#nama-item').html("Bakso Kikil")
+    $('#harga-item').html('Rp. 20.000')
+    $('#item-desc').html("Komplit")
+})
+
+$('#kecap-gurame').on('click', function(){
+    $('#modal-img').attr('src', 'assets/img/gurame-kecap.jpeg')
+    $('#nama-item').html("Gurame Bakar Kecap")
+    $('#harga-item').html('Rp. 20.000')
+    $('#item-desc').html("Komplit")
+})
+
+$('#nasi-bakar').on('click', function(){
+    $('#modal-img').attr('src', 'assets/img/nasi-bakar.jpg')
+    $('#nama-item').html("Bakso Kikil")
+    $('#harga-item').html('Rp. 20.000')
+    $('#item-desc').html("Komplit")
+})
